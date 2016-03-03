df4919dfe4cb5bf0d4eec4752ad7309a3b79b925
Tim Mecklem <tim@teamgaslight.com>
2016-03-03 16:04:29 -0500

    Capture keypresses

--- lib/elixir_matrix_keypad.ex
+++ lib/elixir_matrix_keypad.ex
@@ -1,33 +1,25 @@
 use Bitwise
 
 defmodule ElixirMatrixKeypad do
+  @row_masks [0x7f, 0xbf, 0xdf, 0xef]
   def start_link do
     receive do
       {sender, device, i2c_address} ->
         {:ok, keypad} = I2c.start_link(device, i2c_address)
-        scan(keypad, sender)
+        initial_state = [<<0::size(4)>>, <<0::size(4)>>, <<0::size(4)>>, <<0::size(4)>>]
+        loop_scan(keypad, sender, initial_state)
     end
   end
 
-  defp scan(keypad, sender) do
-    scan(keypad, sender, [<<0::size(4)>>,<<0::size(4)>>,<<0::size(4)>>,<<0::size(4)>>])
-  end
-
-  defp scan(keypad, sender, previous_state) do
+  defp loop_scan(keypad, sender, previous_state) do
     bitmap = scan_matrix(keypad)
-    cond do
-      row_changed?(bitmap, previous_state) -> send(sender, bitmap)
-      true -> nil
-    end
+    if(row_changed?(bitmap, previous_state), do: send(sender, bitmap))
     :timer.sleep 20
-    scan(keypad, sender, bitmap)
+    loop_scan(keypad, sender, bitmap)
   end
 
   defp scan_matrix(keypad) do
-    row_masks = [0x7f, 0xbf, 0xdf, 0xef]
-    Enum.map(row_masks, fn (row_mask) ->
-      scan_row(keypad, row_mask)
-    end)
+    @row_masks |> Enum.map(fn (row_mask) -> scan_row(keypad, row_mask) end)
   end
 
   defp scan_row(keypad, row_mask) do
@@ -36,37 +28,61 @@ defmodule ElixirMatrixKeypad do
     <<presses::size(4)>>
   end
 
-  defp row_changed?([], []), do: false
-  defp row_changed?([row | tail], [previous_row | previous_tail]) do
-    cond do
-      key_changed?(row, previous_row) -> true
-      true -> row_changed?(tail, previous_tail)
+  defp row_changed?([], _), do: false
+  defp row_changed?([<<row::size(4)>> | tail], [<<previous_row::size(4)>> | previous_tail]) do
+    key_changed = row ^^^ previous_row !== 0
+    case key_changed do
+      true -> true
+      false -> row_changed?(tail, previous_tail)
     end
-  end
-
-  defp key_changed?(<<row::4>>, <<previous_row::4>>) do
-    row ^^^ previous_row !== 0
   end
 end
 
-defmodule KeypadScanner do
+defmodule CharacterKeypad do
+  @keymap [
+    '123A',
+    '456B',
+    '789C',
+    '*0#D'
+  ]
+
   def start do
-    pid = spawn(ElixirKeypad, :start_link, [])
+    pid = spawn(ElixirMatrixKeypad, :start_link, [])
     send pid, {self, "i2c-1", 0x20}
-    keep_scanning
+    monitor_matrix
   end
 
-  defp keep_scanning do
+  @initial_state [<<0::size(4)>>, <<0::size(4)>>, <<0::size(4)>>, <<0::size(4)>>]
+  defp monitor_matrix(previous_matrix \\ @initial_state) do
     receive do
-      matrix -> print_matrix(matrix)
+      matrix ->
+        keydowns = new_keydowns(matrix, previous_matrix, @keymap)
+        if(length(keydowns) > 0, do: :io.format("~w", [List.to_string(keydowns)]))
+        monitor_matrix(matrix)
     end
-    keep_scanning
   end
 
-  defp print_matrix([]), do: nil
-  defp print_matrix([row | tail]) do
-    <<bits::4>> = row
-    :io.format("~4.2.0B~n", [bits])
-    print_matrix(tail)
+  defp new_keydowns(matrix, previous_matrix, keymap), do: _new_keydowns(matrix, previous_matrix, keymap, [])
+  defp _new_keydowns([], _, _, keypresses), do: keypresses
+  defp _new_keydowns([<<row::size(4)>> | tail], [<<previous_row::size(4)>> | previous_tail], [row_map | keymap_tail], keypresses) do
+    keypresses = ((previous_row ^^^ row) &&& row) |> map_row_keys(row_map, keypresses)
+    _new_keydowns(tail, previous_tail, keymap_tail, keypresses)
   end
+
+  defp map_row_keys(_, [], keypresses), do: keypresses
+  defp map_row_keys(row, [key | tail], keypresses) do
+    char_pressed = (row &&& 0x08) !== 0
+    keypresses = case char_pressed do
+                    true -> [key | keypresses]
+                    false -> keypresses
+                  end
+    map_row_keys(row <<< 1, tail, keypresses)
+  end
+
+  # defp print_matrix([]), do: nil
+  # defp print_matrix([row | tail]) do
+  #   <<bits::4>> = row
+  #   :io.format("~4.2.0B~n", [bits])
+  #   print_matrix(tail)
+  # end
 end
